6a0dd6b68be3a3aa15e4b3059d39b06d6c94576d
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-05-01 18:04:47 +0600

    added qzone files

--- src/Components/FindUs/FindUs.jsx
+++ src/Components/FindUs/FindUs.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 const FindUs = () => {
     return (
         <div>
-            
+            <h3 className='font-bold mb-5'>Find Us On</h3>
+            <div>
+                <div className="join join-vertical w-full">
+                    <button className="btn bg-base-100 justify-start join-item">
+                        <FaFacebook size={16} />
+                        Facebook</button>
+                    <button className="btn bg-base-100 justify-start join-item">
+                        <FaTwitter size={16} />
+                        Twitter</button>
+                    <button className="btn bg-base-100 justify-start join-item">
+                        <FaInstagram size={16} />
+                        Instagram</button>
+                </div >
+            </div>
         </div>
     );
 };
--- src/Components/RightAside/RightAside.jsx
+++ src/Components/RightAside/RightAside.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ScosalLogin from '../ScosalLogin/ScosalLogin';
+import FindUs from '../FindUs/FindUs';
 
 const RightAside = () => {
     return (
-        <div>
+        <div className='space-y-5'>
             <ScosalLogin />
+            <FindUs />
         </div>
     );
 };
